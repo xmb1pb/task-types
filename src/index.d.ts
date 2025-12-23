306093781c36066b3c7abb0cad65805026db2a23
xmb1pb <mzharavin@gmail.com>
2025-12-23 11:50:15 +0700

    task1whatever

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,7 +4,16 @@
 - age - число
 - gender - 'male' или 'female'
  */
-export function getPersons();
+
+type Person = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+};
+
+type Employee = Person & { company: string };
+
+export function getPersons(): Person[];
 
 /*
 Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
@@ -18,4 +27,4 @@ export function getPersons();
    - gender - 'male' или 'female'
    - company- строка
  */
-export function personToString(person);
+export function personToString(person: Person | Employee): string;
